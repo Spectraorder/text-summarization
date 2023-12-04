7611e0655eee0c1e1a972d93796e1ecb05d14dea
NickNameInvalid <zssmh98@outlook.com>
2023-12-04 00:44:25 -0500

    update

--- text_rank/langauge_model.py
+++ text_rank/langauge_model.py
@@ -1,6 +1,7 @@
 from transformers import BartTokenizerFast, BartForConditionalGeneration
 from text_rank.evaluation import *
 import torch
+from .config import *
 
 class language_model:
     def __init__(self, model_name: str, weight_path: str):
@@ -16,7 +17,7 @@ class language_model:
         prompt_text = self.prefix + str(text)
         inputs = self.tokenizer([prompt_text], max_length=1024, return_tensors='pt', truncation=True)
         inputs.to(self.device)
-        summary_ids = self.model.generate(inputs['input_ids'], num_beams=4, min_length=100, max_length=200,
+        summary_ids = self.model.generate(inputs['input_ids'], num_beams=4, min_length=MIN_MODEL_GEN, max_length=MAX_MODEL_GEN,
                                      early_stopping=False)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return summary
