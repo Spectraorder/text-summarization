7021cca66b86b6e76496275bb3518a688f9ca9d6
NickNameInvalid <zssmh98@outlook.com>
2023-12-03 23:59:10 -0500

    update cuda for language model

--- text_rank/langauge_model.py
+++ text_rank/langauge_model.py
@@ -10,12 +10,14 @@ class language_model:
         self.model_name = model_name
         self.tokenizer = BartTokenizerFast.from_pretrained(model_name)
         self.model = BartForConditionalGeneration.from_pretrained(self.weight_path)
+        self.model.to(self.device)
 
     def generate_summarisation(self, text: str):
         prompt_text = self.prefix + str(text)
         inputs = self.tokenizer([prompt_text], max_length=1024, return_tensors='pt', truncation=True)
-        summary_ids = self.model.generate(inputs['input_ids'], num_beams=4, min_new_tokens=50, max_length=120,
-                                     early_stopping=True)
+        inputs.to(self.device)
+        summary_ids = self.model.generate(inputs['input_ids'], num_beams=4, min_length=100, max_length=200,
+                                     early_stopping=False)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return summary
 
